fecd80514726b63a903f6ca92a36f38773e9b3ce
Droplet <darellpetualang@gmail.com>
2024-03-08 22:57:21 +0700

    tweaks

--- resources/views/guest/gallery.blade.php
+++ resources/views/guest/gallery.blade.php
@@ -11,6 +11,21 @@
     
 @section('content')
 
+<div class="p-3">
+    
+    <div class="pagetitle">
+        <h1 style="color:#1b31fc;">Gallery</h1>
+    </div>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item ">
+                <a href="{{ url('/') }}">Home</a>
+            </li>
+            <li class="breadcrumb-item active"><a href="{{ url('/guest/gallery') }}">Gallery</a></li>
+        </ol>
+    </nav>
+
+</div>
 @endsection
 </body>
 </html>
